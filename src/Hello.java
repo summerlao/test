b877a7a52947e7cdeecde99de0d95ae49f8f484c
Hongyu Lao <summer@procurify.com>
2016-10-13 19:08:15 -0700

    f2

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello 3");
-		System.out.println("feature-1 1");
+		System.out.println("feature-1 2");
 	}
 
 }
